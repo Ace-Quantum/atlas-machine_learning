308f187471d0ed76cc2574e4e8a8a953d12ebdae
Ace <acequantumbit@gmail.com>
2024-08-06 13:22:00 -0500

    Made file for 4

---
+++ supervised_learning/object_detection/4-yolo.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+"""Set up for the Yolo Model"""
+
+from tensorflow import keras as K
+
+
+def load_class_names(filepath):
+    """Load up the classes"""
+    with open(filepath, "r") as file:
+        class_names = file.readlines()
+    return [name.strip() for name in class_names]
+
+
+class Yolo:
+    """Yolo Class"""
+
+    def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
+        """initialization of yolo"""
+        self.model = K.models.load_model(model_path)
+        self.class_names = load_class_names(classes_path)
+        self.class_t = class_t
+        self.nms_t = nms_t
+        self.anchors = anchors
+
+    def process_outputs(self, outputs, image_size):
+        """Processes the outputs"""
