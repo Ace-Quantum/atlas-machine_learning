55b28546457da4955c93a7d78fc1d78cf30fdf24
Ace <acequantumbit@gmail.com>
2024-08-07 14:41:31 -0500

    Ok I think this is it?

--- supervised_learning/object_detection/1-yolo.py
+++ supervised_learning/object_detection/1-yolo.py
@@ -24,6 +24,9 @@ class Yolo:
         self.nms_t = nms_t
         self.anchors = anchors
 
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+
     def process_outputs(self, outputs, image_size):
         """Processes the outputs"""
         # Pull the dimensions of the image
@@ -36,23 +39,40 @@ class Yolo:
 
         # for each output
         for output in outputs:
-            # Grab box coordinates
-            processed_boxes = output[:, :, :, :4]
 
-            # convert coordinates
-            processed_boxes[..., :2] = (processed_boxes[..., :2] + np.indices((
-                output.shape[0], output.shape[1])).T) / (output.shape[0], output.shape[1])
-            
-            processed_boxes[..., 2:4] = np.exp(processed_boxes[..., 2:4]) * self.model.anchors / image_size
+            # Grab grid vars
+            grid_height, grid_width, anchor_boxes, _ = output.shape
 
-            # extract confidences
-            box_confidence = output[:, :, :, 4:5]
+            box_xy = self.sigmoid(output[..., :2])
 
-            # extract class probabilities
-            box_class_prob = output[:, :, :, 5:]
+            box_wh = np.exp(output[..., 2:4])
 
-            boxes.append(processed_boxes)
+            box_confidence = self.sigmoid(output[..., 4:5])
+
+            box_class_prob = self.sigmoid(output[..., 5:])
+
+            # Creating new grid
+            col = np.tile(np.arange(0, grid_width), grid_height).reshape(-1, grid_width)
+            row = np.tile(np.arange(0, grid_height).reshape(-1, 1), grid_width)
+            col = col.reshape(grid_height, grid_width, 1, 1).repeat(anchor_boxes, axis=-2)
+            row = row.reshape(grid_height, grid_width, 1, 1).repeat(anchor_boxes, axis=-2)
+
+            box_xy += np.concatenate((col, row), axis=-1)
+
+            box_xy /= (grid_width, grid_height)
+            box_wh /= self.model.input.shape[1:3]
+
+            box_xy -= (box_wh / 2)
+
+            boxes.append(np.concatenate((box_xy, box_xy + box_wh), axis=-1))
             box_confidences.append(box_confidence)
             box_class_probs.append(box_class_prob)
+
+        # scale boxes back to the original
+        for i in range(len(boxes)):
+            boxes[i][..., 0] *= image_width
+            boxes[i][..., 1] *= image_height
+            boxes[i][..., 2] *= image_width
+            boxes[i][..., 3] *= image_height
 
         return boxes, box_confidences, box_class_probs
