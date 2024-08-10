48145ad36699b182e748063f7aee55d937c36a6c
Ace <acequantumbit@gmail.com>
2024-08-10 14:09:41 -0500

    this isn't right but I'm pushing it anyway

--- supervised_learning/object_detection/1-yolo.py
+++ supervised_learning/object_detection/1-yolo.py
@@ -40,23 +40,39 @@ class Yolo:
         box_class_probs = []
 
         # for each output
-        for output in outputs:
+        for i in range(len(outputs)):
 
             # Grab grid vars
-            grid_height, grid_width, anchor_boxes, _ = output.shape
+            grid_height, grid_width = outputs[i].shape[:2]
+            anchors = self.anchors[i]
 
-            box_confidence = self.sigmoid(output[..., 4:5])
-
-            box_class_prob = self.sigmoid(output[..., 5:])
+            # Confidences and probabilities
+            box_confidence = self.sigmoid(outputs[i][..., 4:5])
+            box_class_prob = self.sigmoid(outputs[i][..., 5:])
 
             box_confidences.append(box_confidence)
             box_class_probs.append(box_class_prob)
 
-        # scale boxes back to the original
-        for i in range(len(boxes)):
-            boxes[i][..., 0] *= image_width
-            boxes[i][..., 1] *= image_height
-            boxes[i][..., 2] *= image_width
-            boxes[i][..., 3] *= image_height
+            # Box Things
+            pred_cent = outputs[i][..., :2]
+            pred_h_w = outputs[i][..., 2:4]
+
+            # calculating normalized width and height 
+            norm_box_h_w = anchors * np.exp(pred_h_w)
+            norm_box_h_w /=[self.model.input[0].shape[1], self.model.input[0].shape[2]]
+
+            # calculate coordinates
+            grid = np.tile(np.indices((grid_width, grid_height)).T,
+                           anchors.shape[0]).reshape(grid_height, grid_width, -1, 2)
+            
+            act_xy = (self.sigmoid(pred_cent) + grid) / [grid_width, grid_height]
+            act_xy1 = act_xy - (norm_box_h_w / 2)
+            act_xy2 = act_xy1 + (norm_box_h_w / 2)
+
+            box = np.concatenate((act_xy1, act_xy2), axis=-1)
+
+            box *= np.tile(image_size, 2)
+
+            boxes.append(box)
 
         return boxes, box_confidences, box_class_probs
