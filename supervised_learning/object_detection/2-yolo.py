9b0cdd95f91f00d67bc41c0ef9b71d41f7492a7d
Ace <acequantumbit@gmail.com>
2024-08-15 22:38:25 -0500

    I wish I could say first try lol

--- supervised_learning/object_detection/2-yolo.py
+++ supervised_learning/object_detection/2-yolo.py
@@ -24,5 +24,86 @@ class Yolo:
         self.nms_t = nms_t
         self.anchors = anchors
 
+    def sigmoid(self, x):
+        """Makes a sigmoid to return
+        Idk if I'll need it"""
+        return 1 / (1 + np.exp(-x))
+
     def process_outputs(self, outputs, image_size):
         """Processes the outputs"""
+        boxes = [output[..., :4] for output in outputs]
+        box_confidences = []
+        box_class_probs = []
+
+        image_height = image_size[0]
+        image_width = image_size[1]
+
+        input_layer = self.model.input
+        input_width, input_height = input_layer.shape[1:3]
+
+        for i, output in enumerate(outputs):
+
+            box = output
+            box_x = box[..., 0]
+            box_y = box[..., 1]
+            box_w = box[..., 2]
+            box_h = box[..., 3]
+
+            box_confidence = self.sigmoid(output[..., 4:5])
+            box_class_prob = self.sigmoid(output[..., 5:])
+
+            box_confidences.append(box_confidence)
+            box_class_probs.append(box_class_prob)
+
+            grid_height = box.shape[0]
+            grid_width = box.shape[1]
+            num_anchors = output.shape[2]
+
+            center_x = np.arange(grid_width).reshape(1, grid_width)
+            center_x = np.repeat(center_x, grid_height, axis=0)
+
+            center_y = np.arange(grid_width).reshape(1, grid_width)
+            center_y = np.repeat(center_y, grid_height, axis=0).T
+
+            center_x = np.repeat(center_x[..., np.newaxis],
+                                 num_anchors, axis=2)
+            center_y = np.repeat(center_y[..., np.newaxis],
+                                 num_anchors, axis=2)
+
+            pred_x = (self.sigmoid(box_x) + center_x) / grid_width
+            pred_y = (self.sigmoid(box_y) + center_y) / grid_height
+            pred_w = (np.exp(box_w) * self.anchors[i, :, 0]) / input_width
+            pred_h = (np.exp(box_h) * self.anchors[i, :, 1]) / input_height
+
+            boxes[i][..., 0] = (pred_x - (pred_w / 2)) * image_width
+            boxes[i][..., 1] = (pred_y - (pred_h / 2)) * image_height
+            boxes[i][..., 2] = (pred_x + (pred_w / 2)) * image_width
+            boxes[i][..., 3] = (pred_y + (pred_h / 2)) * image_height
+
+            # boxes.append(box)
+
+        return boxes, box_confidences, box_class_probs
+
+    def filter_boxes(self, boxes, box_confidences, box_class_probs):
+        filtered_boxes = []
+        box_classes = []
+        box_scores = []
+
+        for i in range(len(boxes)):
+            curr_boxes = boxes[i]
+            curr_confidences = box_confidences[i]
+            curr_class_probs = box_class_probs[i]
+
+            scores = curr_confidences * curr_class_probs
+            max_scores = np.max(scores, axis=-1)
+            mask = max_scores >= self.class_t
+
+            filtered_boxes.append(curr_boxes[mask])
+            box_classes.append(np.argmax(curr_class_probs[mask], axis=-1))
+            box_scores.append(scores[mask])
+
+        filtered_boxes = np.concatenate(filtered_boxes, axis=0)
+        box_classes = np.concatenate(box_classes, axis=0)
+        box_scores = np.concatenate(box_scores, axis=0)
+
+        return filtered_boxes, box_classes, box_scores
