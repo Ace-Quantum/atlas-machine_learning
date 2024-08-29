8f9537c4bc0cc0e6cfcae279593549a15b2b8be7
Ace <acequantumbit@gmail.com>
2024-08-29 14:28:24 -0500

    Trying this out

---
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""This will find the definiteness of a matrix"""
+
+import numpy as np
+
+def valid_matrix(matrix):
+    """Checks if the matrix is a list of lists"""
+
+    if not isinstance(matrix, list):
+        None
+
+    if not all(isinstance(row, list) for row in matrix):
+        None
+
+    if not matrix:
+        None
+
+    row_len = len(matrix)
+
+    for row in matrix:
+        if len(row) != row_len:
+            None
+
+def definiteness(matrix):
+    """Returns the definiteness of a marix"""
+    
+    if not isinstance(matrix, np.ndarray):
+        return TypeError("matrix must be a numpy.ndarray")
+    
+    
