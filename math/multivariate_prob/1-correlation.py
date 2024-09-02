58592edd9915072e3944ec1fda67480b7001c6b5
Ace <acequantumbit@gmail.com>
2024-09-02 13:05:55 -0500

    Let's see if it works

--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -16,3 +16,16 @@ def correlation(C):
 
     if C.shape != (dim, dim):
         raise ValueError("C must be a 2D square matrix")
+
+    # ret_matrix = C.corrcoef()
+
+    ret_matrix = np.zeroes((dim, dim))
+
+    for i in range(dim):
+        for j in range(dim):
+            if i==j:
+                ret_matrix[i, j] = 1
+            else:
+                ret_matrix[i, j] = C[i, j] / np.sqrt(C[i, i] * C[j, j])
+
+    return ret_matrix
