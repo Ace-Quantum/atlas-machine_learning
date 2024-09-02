9de8fc3a550378ecf5ab24b9bf1cdfd4e49db332
Ace <acequantumbit@gmail.com>
2024-09-02 10:06:04 -0500

    I at least have getting the mean right:

---
+++ math/multivariate_prob/0-mean_cov.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""This is a module for finding the mean and covariance 
+of a data set in NumPy"""
+
+import numpy as np
+
+
+
+def mean_cov(X):
+    """The function that runs the module"""
+
+    # num_data = X[0]
+    # X_means = []
+
+    # for i in range(len(num_data)):
+    #     X_means.append(np.mean(X[i]))
+
+    X_mean = np.mean(X, axis=0)
+    X_cov = np.cov(X, ddof=1)
+
+    return (X_mean, X_cov)
