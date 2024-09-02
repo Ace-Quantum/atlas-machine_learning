32eda2bc0f1fb8c0cb2153601f87f5c5de8a8032
Ace <acequantumbit@gmail.com>
2024-09-02 11:16:50 -0500

    Ok why is my terminal unable to run my code?

--- math/multivariate_prob/0-mean_cov.py
+++ math/multivariate_prob/0-mean_cov.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env python3
 """This is a module for finding the mean and covariance 
-of a data set in NumPy"""
+of a data set in NumPy
+This module uses code found in the following link:
+https://python.plainenglish.io/covariance-calculation-using-python-45b6a8e5df9f"""
 
 import numpy as np
 
+def cov_val(x, y):
+    """Helper to find the covariance"""
+    
+    mean_x = sum(x) / float(len(x))
+    mean_y = sum(y) / float(len(y))
 
+    sub_x = [i - mean_x for i in x]
+    sub_y = [i - mean_y for i in y]
+
+    sum_value = sum([sub_y[i]*sub_x[i] for i in range(len(x))])
+    denom = float(len(x)-1)
+
+    cov = sum_value/denom
+    return cov
 
 def mean_cov(X):
     """The function that runs the module"""
-
-    # num_data = X[0]
-    # X_means = []
-
-    # for i in range(len(num_data)):
-    #     X_means.append(np.mean(X[i]))
 
     if not isinstance(X, np.ndarray) or X.ndim != 2:
         raise TypeError("X must be a 2D numpy.ndarray")
@@ -23,7 +32,8 @@ def mean_cov(X):
 
     X_mean = np.mean(X, axis=0)
 
-    # X_mean = np.ndarray(X_mean)
-    X_mean = np.asanyarray(X_mean)
+    X_mean = np.asarray(X_mean)
 
-    return (X_mean, None)
+    X_cov = [[cov_val(a,b) for a in X] for b in X]
+
+    return (X_mean, X_cov)
