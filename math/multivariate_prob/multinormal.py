a6de0c46e3fc5f69956a683733f4002dfd95546e
Ace <acequantumbit@gmail.com>
2024-09-02 12:38:59 -0500

    testing error handling for task 2

---
+++ math/multivariate_prob/multinormal.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""This will be used to represent a Multivariate Normal Distribution"""
+
+import numpy as np
+
+class MultiNormal:
+    def __init__(self, data):
+        if not isinstance(data, np.ndarray):
+            raise TypeError("data must be a 2D numpy.ndarray")
+        
+        if len(data[1]) < 2:
+            raise ValueError("data must contain multiple data points")
