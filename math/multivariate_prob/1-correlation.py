a3671b22370eb025e38462db268d5b6898858357
Ace <acequantumbit@gmail.com>
2024-09-02 12:11:12 -0500

    Testing error handling

--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+"""This module finds the correlation of a matrix"""
+
+import numpy as np
+
+def correlation(C):
+    """This is the function that finds correlation"""
+
+    if not isinstance(C, np.ndarray):
+        raise TypeError("C must be a numpy.ndarray")
+
+    if len(C) != len(C[0]):
+        raise ValueError("C must be a 2D square matrix")
