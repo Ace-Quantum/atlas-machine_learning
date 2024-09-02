be81aa810f83eb0b4991d6ed2d58243d308001b9
Ace <acequantumbit@gmail.com>
2024-09-02 11:37:31 -0500

    Finally done with task 0

--- math/multivariate_prob/0-mean_cov.py
+++ math/multivariate_prob/0-mean_cov.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
-"""This is a module for finding the mean and covariance 
+"""This is a module for finding the mean and covariance
 of a data set in NumPy
 This module uses code found in the following link:
-https://python.plainenglish.io/covariance-calculation-using-python-45b6a8e5df9f"""
+https://python.plainenglish.io/
+covariance-calculation-using-python-45b6a8e5df9f"""
 
 import numpy as np
 
+
 def cov_val(x, y):
     """Helper to find the covariance"""
-    
+
     mean_x = np.mean(x)
     mean_y = np.mean(y)
 
@@ -19,12 +21,13 @@ def cov_val(x, y):
 
     return cov
 
+
 def mean_cov(X):
     """The function that runs the module"""
 
     if not isinstance(X, np.ndarray) or X.ndim != 2:
         raise TypeError("X must be a 2D numpy.ndarray")
-    
+
     if len(X) < 2:
         raise ValueError("X must contain multiple data points")
 
