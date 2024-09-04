62940e245c21e3e0770eef96f3e4c36b2fc2a5cb
Ace <acequantumbit@gmail.com>
2024-09-04 12:55:51 -0500

    Error handling first task

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -8,7 +8,10 @@ def likelihood(x, n, P):
     """determine likelihood"""
 
     # Verifications
-    if not n >= 0 or not isinstance(n, float):
+    if not isinstance(n, float):
+        raise ValueError("n must be a positive integer")
+
+    if  not n >= 0:
         raise ValueError("n must be a positive integer")
 
     if x <= 0:
