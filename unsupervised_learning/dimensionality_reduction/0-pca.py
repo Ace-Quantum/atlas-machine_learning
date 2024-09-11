c50231405920516aa8b1094cdd35ea3a9b527dad
Ace <acequantumbit@gmail.com>
2024-09-11 15:58:40 -0500

    No idea what I'm supposed to do but at least I'm styled

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """Here's some documentation
 I'm using code from this website as a reference
-https://medium.com/@nahmed3536/a-python-implementation-of-pca-with-numpy-1bbd3b21de2e"""
+https://medium.com/@nahmed3536/
+a-python-implementation-of-pca-with-numpy-1bbd3b21de2e"""
 
 import numpy as np
 
@@ -10,7 +11,7 @@ def pca(X, var=0.95):
     """And some more documentation
     I genuinely don't know what I'm doing here"""
 
-    cov_matrix = np.cov(X, ddof = 1, rowvar= False)
+    cov_matrix = np.cov(X, ddof=1, rowvar=False)
 
     eigenvals, eigenvects = np.linalg.eig(cov_matrix)
 
