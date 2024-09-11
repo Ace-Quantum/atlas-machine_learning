ffc699b6d2b45f30c8bf0fd4db5427eb4bd02704
Ace <acequantumbit@gmail.com>
2024-09-11 15:31:23 -0500

    I'm not sure if it's right but I'm sending it

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -1,9 +1,29 @@
 #!/usr/bin/env python3
-"""Here's some documentation"""
+"""Here's some documentation
+I'm using code from this website as a reference
+https://medium.com/@nahmed3536/a-python-implementation-of-pca-with-numpy-1bbd3b21de2e"""
 
 import numpy as np
 
 
 def pca(X, var=0.95):
-    """And some more documentation"""
-    return None
+    """And some more documentation
+    I genuinely don't know what I'm doing here"""
+
+    cov_matrix = np.cov(X, ddof = 1, rowvar= False)
+
+    eigenvals, eigenvects = np.linalg.eig(cov_matrix)
+
+    ord_of_imp = np.argsort(eigenvals)[::-1]
+
+    sort_eigenvals = eigenvals[ord_of_imp]
+    sort_eigenvects = eigenvects[:, ord_of_imp]
+
+    exp_var = sort_eigenvals / np.sum(sort_eigenvals)
+
+    cum_var = np.cumsum(exp_var)
+    k = np.argmax(cum_var >= var) + 1
+
+    W = sort_eigenvects[:, :k]
+
+    return W
