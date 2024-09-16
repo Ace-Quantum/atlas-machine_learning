2293ec1226282d91c765a102eb9656100d244abe
Ace <acequantumbit@gmail.com>
2024-09-16 15:07:52 -0500

    I'm not actually sure what I'm doing

--- unsupervised_learning/clustering/2-variance.py
+++ unsupervised_learning/clustering/2-variance.py
@@ -6,4 +6,17 @@ import numpy as np
 
 def variance(X, C):
     """Documentation"""
-    return None
+    if not isinstance(X, np.ndarray) or not isinstance(C, np.ndarray):
+        return None
+    if X.ndim != 2 or C.ndim != 2:
+        return None
+    if X.shape[1] != C.shape[1]:
+        return None
+    
+    distances = np.linalg.norm(X[:, np.newaxis] - C, axis=2) ** 2
+
+    min_dis = np.min(distances, axis=1)
+
+    var = np.sum(min_dis)
+
+    return var
