a7ab3edcb41b0a8fd626f4d4e192e793b21fbc95
Ace <acequantumbit@gmail.com>
2024-09-16 13:35:55 -0500

    Still confused but at least there's output

--- unsupervised_learning/clustering/1-kmeans.py
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -2,8 +2,32 @@
 """Here's some documentation"""
 
 import numpy as np
+initialize = __import__('0-initialize').initialize
 
 
 def kmeans(X, k, iterations=1000):
     """Documentation"""
-    return None
+    
+    C = initialize(X, k)
+    if C is None:
+        return None, None
+    
+    n, d = X.shape
+    
+    for _ in range(iterations):
+        clss = np.argmin(np.linalg.norm(X[:, np.newaxis] - C, axis=2), axis=1)
+
+        # calculating new centroids
+        new_C = np.array([X[clss == i].mean(axis=0) if np.any(
+            clss ==i) else C[i] for i in range(k)])
+
+        if np.all(C == new_C):
+            break
+
+        C = new_C
+
+    # return_clusters = ~np.all(new_C == 0, axis=1)
+    # C = C[return_clusters]
+    # clss = clss[return_clusters]
+
+    return C, clss
