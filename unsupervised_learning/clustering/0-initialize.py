576faffe8a83598f596a1102aba5bd334e2fa569
Ace <acequantumbit@gmail.com>
2024-09-16 13:01:54 -0500

    So far so good

--- unsupervised_learning/clustering/0-initialize.py
+++ unsupervised_learning/clustering/0-initialize.py
@@ -6,4 +6,19 @@ import numpy as np
 
 def initialize(X, k):
     """Documentation"""
-    return None
+    
+    if not isinstance(X, np.ndarray):
+        return None
+    if not isinstance(k, int) or k <= 0:
+        return None
+    
+    n, d = X.shape
+    if n == 0 or d == 0:
+        return None
+    
+    min_values = X.min(axis=0)
+    max_values = X.max(axis=0)
+
+    centroids = np.random.uniform(min_values, max_values, size=(k, d))
+
+    return centroids
