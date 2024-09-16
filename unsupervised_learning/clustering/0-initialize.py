9ec04bd1f9818c92436c9cbcfb1134839f62a38b
Ace <acequantumbit@gmail.com>
2024-09-16 13:06:27 -0500

    styling

--- unsupervised_learning/clustering/0-initialize.py
+++ unsupervised_learning/clustering/0-initialize.py
@@ -6,18 +6,18 @@ import numpy as np
 
 def initialize(X, k):
     """Documentation"""
-    
+
     if not isinstance(X, np.ndarray):
         return None
     if not isinstance(k, int) or k <= 0:
         return None
     if len(X.shape) != 2:
         return None
-    
+
     n, d = X.shape
     if n == 0 or d == 0:
         return None
-    
+
     min_values = X.min(axis=0)
     max_values = X.max(axis=0)
 
