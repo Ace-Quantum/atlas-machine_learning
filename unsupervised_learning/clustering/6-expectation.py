e603c0c6541208e6ea2cdfd73dfa44742c19c39e
Ace <acequantumbit@gmail.com>
2024-09-17 15:24:33 -0500

    The main uses task 4 which I can't do because it needs task 1 and that's been failing for me since last week

--- unsupervised_learning/clustering/6-expectation.py
+++ unsupervised_learning/clustering/6-expectation.py
@@ -2,8 +2,26 @@
 """Here's some documentation"""
 
 import numpy as np
+pdf = __import__('5-pdf').pdf
 
 
 def expectation(X, pi, m, S):
     """Documentation"""
-    return None
+    n, d = X.shape
+    k = pi.shape[0]
+
+    g = np.zeros((k, n))
+
+    for i in range(k):
+        g[i] = pi[i] * pdf(X, m[i], S[i])
+
+    total_probs = np.sum(g, axis=0)
+
+    if np.any(total_probs ==0):
+        return None, None
+    
+    g /= total_probs
+
+    log_likelihood = np.sum(np.log(total_probs))
+
+    return g, log_likelihood
