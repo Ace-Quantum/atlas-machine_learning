4ab2b5118fbf5092cbf970a689d9b92ad38b5fb7
Ace <acequantumbit@gmail.com>
2024-09-18 20:00:23 -0500

    error handling

--- unsupervised_learning/clustering/3-optimum.py
+++ unsupervised_learning/clustering/3-optimum.py
@@ -9,4 +9,22 @@ variance = __import__('2-variance').variance
 
 def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     """Documentation"""
-    return None
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None, None
+    
+    if not isinstance(kmin, int) or kmin <= 0:
+        return None, None
+    
+    if kmax is not None and (not isinstance(kmax, int) or kmax <= kmin):
+        return None, None
+    
+    if not isinstance(iterations, int) or iterations <= 0:
+        return None, None
+    
+    if kmax is None:
+        kmax = X.shape[0]
+
+    if kmax <= kmin:
+        return None, None
+    
+    return None, None
