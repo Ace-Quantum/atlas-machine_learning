e4824180b2a73f8642f7d738fdcd8ca665a44892
Ace <acequantumbit@gmail.com>
2024-09-18 19:37:57 -0500

    styling

--- unsupervised_learning/clustering/10-kmeans.py
+++ unsupervised_learning/clustering/10-kmeans.py
@@ -3,12 +3,14 @@
 
 import sklearn.cluster
 
+
 def kmeans(X, k):
+    """Here have some documentation"""
     # if not isinstance(X, np.ndarray) or len(X.shape) != 2:
     #     return None, None
     if not isinstance(k, int) or k <= 0 or k > X.shape[0]:
         return None, None
-    
+
     kmeans_model = sklearn.cluster.KMeans(n_clusters=k)
 
     kmeans_model.fit(X)
@@ -18,4 +20,3 @@ def kmeans(X, k):
     clss = kmeans_model.labels_
 
     return C, clss
-    
