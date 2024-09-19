a03ae88652fbf84dd03d2175f5f9f7c1e77fb1b3
Ace <acequantumbit@gmail.com>
2024-09-18 19:18:10 -0500

    forgot the import

--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -2,6 +2,7 @@
 """Here's some documentation"""
 
 import numpy as np
+
 expectation_maximization = __import__('8-EM').expectation_maximization
 
 
@@ -10,23 +11,23 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
 
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None, None, None
-    
+
     n, d = X.shape
 
     if not isinstance(kmin, int) or kmin <= 0 or kmin > n:
         return None, None, None, None
-    
+
     if kmax is None:
         kmax = n
     elif not isinstance(kmax, int) or kmax < kmin or kmax > n:
         return None, None, None, None
-    
+
     if isinstance(iterations, int) or iterations <= 0:
         return None, None, None, None
 
     if not isinstance(tol, (float, int)) or tol < 0:
         return None, None, None, None
-    
+
     if not isinstance(verbose, bool):
         return None, None, None, None
 
