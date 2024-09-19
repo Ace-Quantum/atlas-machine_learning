fdb304fe567b982a8b197323b75b044c99841599
Ace <acequantumbit@gmail.com>
2024-09-18 20:01:34 -0500

    stylizing

--- unsupervised_learning/clustering/3-optimum.py
+++ unsupervised_learning/clustering/3-optimum.py
@@ -11,20 +11,20 @@ def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     """Documentation"""
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None
-    
+
     if not isinstance(kmin, int) or kmin <= 0:
         return None, None
-    
+
     if kmax is not None and (not isinstance(kmax, int) or kmax <= kmin):
         return None, None
-    
+
     if not isinstance(iterations, int) or iterations <= 0:
         return None, None
-    
+
     if kmax is None:
         kmax = X.shape[0]
 
     if kmax <= kmin:
         return None, None
-    
+
     return None, None
