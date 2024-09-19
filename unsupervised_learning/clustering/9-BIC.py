1d9c1d5e12e11171b1935f29f30ba53b105a2735
Ace <acequantumbit@gmail.com>
2024-09-18 19:16:53 -0500

    forgot the import

--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -2,6 +2,7 @@
 """Here's some documentation"""
 
 import numpy as np
+expectation_maximization = __import__('8-EM').expectation_maximization
 
 
 def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
