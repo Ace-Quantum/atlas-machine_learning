79641fffc4b048571d43e72647434e84db6d3167
Ace <acequantumbit@gmail.com>
2024-09-23 14:36:35 -0500

    This shouldn't be the solution

--- unsupervised_learning/hmm/0-markov_chain.py
+++ unsupervised_learning/hmm/0-markov_chain.py
@@ -1,9 +1,25 @@
 #!/usr/bin/env python3
-"""Documentation"""
+"""This code is based on this tutorial
+https://www.youtube.com/watch?v=WT6jI8UgROI"""
 
 import numpy as np
 
+def matrix_power(M, iter):
+    if iter == 0:
+        return np.identity(len(M))
+    elif iter == 1:
+        return M
+    else:
+        return np.dot(M, matrix_power(M, iter - 1))
 
 def markov_chain(P, s, t=1):
     """Documentation"""
-    return None
+    stepped_matrix = matrix_power(P, t)
+
+    # print(stepped_matrix[0])
+
+    # ret_matrix = np.dot(stepped_matrix)
+
+    # return np.dot(stepped_matrix[:, 0], s)
+
+    return stepped_matrix[0]
