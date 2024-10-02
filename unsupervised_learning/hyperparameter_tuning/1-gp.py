7f6ceb6e2a76a99bd427b5763b0c5fb96bd19c0d
Ace <acequantumbit@gmail.com>
2024-10-02 10:07:33 -0500

    Just the basic set up

---
+++ unsupervised_learning/hyperparameter_tuning/1-gp.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""creates the class for a
+1D Gaussian Distribution"""
+
+import numpy as np
+
+
+class GaussianProcess:
+    """class as defined earlier"""
+
+    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
+        """Initialize variables"""
+        self.X = X_init
+        self.Y = Y_init
+        self.l = l
+        self.sigma_f = sigma_f
+        # vvv to be changed
+        self.K = None
+
+        def kernel(self, X1, X2):
+            """Return covariance matrix"""
+            return None
+        
+        def predict(self, X_s):
+            """predicts the mean and st.dev of points
+            in a Gaussian process"""
+            return None
