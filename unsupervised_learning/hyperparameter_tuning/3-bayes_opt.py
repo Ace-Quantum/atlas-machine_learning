02f1b3edab0616c6f8c768af4e9d4e2c4d2b5dd3
Ace <acequantumbit@gmail.com>
2024-10-03 10:19:00 -0500

    Ok let's just see how we're doing

---
+++ unsupervised_learning/hyperparameter_tuning/3-bayes_opt.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""creates the class for a
+Bayesian Optimization"""
+
+import numpy as np
+
+GP = __import__("2-gp").GaussianProcess
+
+
+class BayesianOptimization:
+    """Beysianbby"""
+
+    def __init__(
+        self,
+        f,
+        X_init,
+        Y_init,
+        bounds,
+        ac_samples,
+        l=1,
+        sigma_f=1,
+        xsi=0.01,
+        minimize=True,
+    ):
+
+        self.f = f
+        self.gp = GP(self)
+        X_s = None
+        xsi = None
+        minimize = None
