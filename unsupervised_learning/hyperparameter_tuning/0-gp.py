6d791b727721d65569e92285255f17767c1535b3
Ace <acequantumbit@gmail.com>
2024-10-08 12:55:00 -0500

    You know what? This makes sense actually

--- unsupervised_learning/hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/hyperparameter_tuning/0-gp.py
@@ -15,23 +15,22 @@ class GaussianProcess:
         self.l = l
         self.sigma_f = sigma_f
         # vvv to be changed
-        self.K = self.kernel(X_init, Y_init)
+        self.K = self.kernel(X_init, X_init)
 
     def kernel(self, X1, X2):
         """Return covariance matrix"""
 
-        # m, n = X1.shape[0], X2.shape[0]
-
-        # diff = X1 - X2.T
-
-        # sq_dist = np.sum(diff ** 2, axis=1)
-
-        # k = self.sigma_f**2 * np.exp(-0.5 / (2 * self.l**2/))
-
         # sq_dist = np.sum(X1**2, 1).reshape(-1, 1) + np.sum(X2**2, 1) - 2 * np.dot(X1, X2.T)
-
-        # sq_dist = np.sum((X1 - X2.T)**2, axis=1)
-
         # return self.sigma_f**2 * np.exp(-0.5 / self.l**2 * sq_dist)
 
-        # return k
+        m, _ = X1.shape
+        n, _ = X2.shape
+
+        k = np.zeros((m, n))
+
+        for i in range(m):
+            for j in range(n):
+                diff = np.linalg.norm(X1[i] - X2[j])
+                k[i, j] = (self.sigma_f ** 2 * np.exp(-(diff**2) / (2 * self.l**2)))
+
+        return k
