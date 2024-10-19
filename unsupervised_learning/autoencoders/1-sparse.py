981055300c4bf3e2d22e0e8565d4396f2c8cfafb
Ace <acequantumbit@gmail.com>
2024-10-19 12:52:57 -0500

    formatting

---
+++ unsupervised_learning/autoencoders/1-sparse.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""sparsity"""
+
+import tensorflow.keras as keras
+
+
+def autoencoder(input_dims, hidden_layers, latent_dims):
+    """Creates a sparse autoencoder"""
+    return None
