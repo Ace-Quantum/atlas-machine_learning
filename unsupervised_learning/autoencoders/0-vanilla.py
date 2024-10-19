bcfd6e4b68e1fe7f9bedc386ee9eb4a45034364b
Ace <acequantumbit@gmail.com>
2024-10-19 12:49:33 -0500

    Formatting and documentation

--- unsupervised_learning/autoencoders/0-vanilla.py
+++ unsupervised_learning/autoencoders/0-vanilla.py
@@ -5,16 +5,18 @@ for the purposes of compressing and decompressing data"""
 
 import tensorflow.keras as keras
 
+
 def autoencoder(input_dims, hidden_layers, latent_dims):
+    """Encodes and decodes"""
     # the encoder protion
     in_layer = keras.Input(shape=(input_dims,))
 
     encoded_layer = in_layer
 
     for n in hidden_layers:
-        encoded_layer = keras.layers.Dense(n, activation='relu')(encoded_layer)
+        encoded_layer = keras.layers.Dense(n, activation="relu")(encoded_layer)
 
-    latent = keras.layers.Dense(latent_dims, activation='relu')(encoded_layer)
+    latent = keras.layers.Dense(latent_dims, activation="relu")(encoded_layer)
 
     encoded_layer = keras.Model(in_layer, latent)
 
@@ -23,9 +25,9 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
 
     decoded = decoded_in_layer
     for n in reversed(hidden_layers):
-        decoded = keras.layers.Dense(n, activation='relu')(decoded)
-    
-    output_layer = keras.layers.Dense(input_dims, activation='sigmoid')(decoded)
+        decoded = keras.layers.Dense(n, activation="relu")(decoded)
+
+    output_layer = keras.layers.Dense(input_dims, activation="sigmoid")(decoded)
 
     decoder = keras.Model(decoded_in_layer, output_layer)
 
@@ -36,6 +38,6 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
 
     autoencoder = keras.Model(auto_input, reconstructed_output)
 
-    autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
+    autoencoder.compile(optimizer="adam", loss="binary_crossentropy")
 
     return encoded_layer, decoder, autoencoder
