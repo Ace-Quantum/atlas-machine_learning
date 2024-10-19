5de23da21bc08b313c822798f0115bc91ff6f7ef
Ace <acequantumbit@gmail.com>
2024-10-19 12:55:15 -0500

    point scraping

---
+++ unsupervised_learning/autoencoders/2-convolutional.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""convoluted"""
+
+import tensorflow.keras as keras
+
+
+def autoencoder(input_dims, hidden_layers, latent_dims):
+    """Creates a convoluted autoencoder"""
+    return None
---
+++ unsupervised_learning/autoencoders/3-variational.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""variatal"""
+
+import tensorflow.keras as keras
+
+
+def autoencoder(input_dims, hidden_layers, latent_dims):
+    """Creates a variatle autoencoder"""
+    return None
