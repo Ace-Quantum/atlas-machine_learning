c44fa500ae2488a93d9dd4a3fb652480024aff95
Ace <acequantumbit@gmail.com>
2024-11-02 13:24:19 -0500

    Committing before switching folders

--- supervised_learning/RNNs/0-rnn_cell.py
+++ supervised_learning/RNNs/0-rnn_cell.py
@@ -10,7 +10,21 @@ class RNNCell:
     than normal NN cells
     But that's what this project aims to teach us"""
     def __init__(self, i, h, o):
-        return None
+        """Initialization
+        i - dimensionality of data
+        h - dimensionality of the hidden state
+        o - dimensionality of outputs
+        Wh - concatinated hidden state/input data weight
+        bh - concatinated hidden state/input data bias
+        Wy - output weight
+        by - output bias
+        Initialized in random normal distribution
+        Weights used on the right side of matrix multiplication
+        Biases initialized to zero"""
+
+        
+        
 
     def forward(self, h_prev, x_t):
+        """Forward propogation"""
         return None
