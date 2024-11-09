371f05cebe672ec14d8ac3b4d07bf2f9e8c64c6c
Ace <acequantumbit@gmail.com>
2024-11-09 17:59:22 -0600

    I've been cooking for 6 hours now

--- Playground/Time-Series/try_two_now.py
+++ Playground/Time-Series/try_two_now.py
@@ -42,4 +42,74 @@ data['Close'].fillna(method='ffill', inplace=True)
 
 # print(data.head())
 
+start = datetime.datetime(2015, 1, 1, 0, 0, 0, 0, pytz.UTC)
+end = datetime.datetime(2018, 11, 11, 0, 0, 0, 0, pytz.UTC)
 
+weekly_rows = data[(data['Timestamp'] >= start) & (data['Timestamp'] <= end)].groupby(
+    [pd.Grouper(key='Timestamp', freq='W-MON')]).first().reset_index()
+# This feels like a code golfing line
+# I'm going to try to make it multiple lines when I do the actual project
+
+# print(weekly_rows.head())
+
+trace1 = go.Scatter(
+    x = weekly_rows['Timestamp'],
+    y = weekly_rows['Open'].astype(float),
+    mode = 'lines',
+    name = 'Open'
+)
+
+trace2 = go.Scatter(
+    x = weekly_rows['Timestamp'],
+    y = weekly_rows['Close'].astype(float),
+    mode = 'lines',
+    name = 'Close'
+)
+
+trace3 = go.Scatter(
+    x = weekly_rows['Timestamp'],
+    y = weekly_rows['Weighted_Price'].astype(float),
+    mode = 'lines',
+    name = 'Weighted Avg'
+)
+
+layout = dict(
+    title='Bitcoin history abriged 2015-2018 w/ slider',
+    xaxis=dict(
+        rangeselector=dict(
+            buttons=list([
+                dict(
+                    count=1,
+                    label='1m',
+                    step='month',
+                    stepmode='backward'
+                ),
+                dict(
+                    count=6,
+                    label='6m',
+                    step='month',
+                    stepmode='backward'
+                ),
+                dict(
+                    count=36,
+                    label='3y',
+                    step='month',
+                    stepmode='backward'
+                ),
+                dict(step='all')
+            ])
+        ),
+        rangeslider=dict(
+            visible = True
+        ),
+        type='date'
+    )
+)
+# Okay that was NOT fun to type
+# Is there an easier way or is that just how formatting is?
+
+data = [trace1, trace2, trace3]
+fig = dict(data=data, layout=layout)
+iplot(fig, filename = "time series ranger")
+
+# fig.show()
