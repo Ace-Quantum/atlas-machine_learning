26be107d2b476ebb53ff9ce653693a8b6b6f8e99
Ace <acequantumbit@gmail.com>
2024-11-09 14:31:23 -0600

    Stashing and pivoting

--- Playground/Time-Series/try_one_i_guess.py
+++ Playground/Time-Series/try_one_i_guess.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+# ****IGNORE THIS ATTEMPT****
+# ****DATA DID NOT LOAD****
+
 import os
 import datetime
 
@@ -20,16 +23,8 @@ zip_path = tf.keras.utils.get_file(
     fname='jena_climate_2009_2016.csv.zip',
     extract=True
 )
-# csv_path, _ = os.path.splitext(zip_path)
 
-# # ------- this section was given to me by chatgpt -------
-# {{{{{{{{This code, in fact, did not work}}}}}}}}
-# csv_path = zip_path.replace('.zip', '.csv')
-
-# if not os.path.isfile(csv_path):
-#     raise FileNotFoundError(f"CSV file not found at {csv_path}")
-
-# # ------- End of AI section -------
+csv_path, _ = os.path.splitext(zip_path)
 
 df = pd.read_csv(csv_path)
 
