6bec1479c2ef52739a4c6d3e0203a227f2ed1b4f
Ace <acequantumbit@gmail.com>
2024-11-13 21:22:40 -0600

    Speedrunning by only testing on the checker

--- supervised_learning/RNNs/4-deep_rnn.py
+++ supervised_learning/RNNs/4-deep_rnn.py
@@ -8,4 +8,21 @@ def deep_rnn(rnn_cells, X, h_0):
     """Looks like we're using the same RNN cells as before
     which is kinda interesting ngl"""
 
-    return None
+    t, m, i = X.shape
+    l = len(rnn_cells)
+    h = h_0.shape[1]
+
+    H = np.zeroes((t, l, m, h))
+    Y = np.zeroes((t, m, l))
+
+    for t_count in range(t):
+        x_t = X[t_count]
+
+        h_next = h_0[:, :, 0]
+        for layer, cell in enumerate(rnn_cells):
+            h_next, y = cell.forward(h_next, None, x_t)
+
+            H[t_count, layer] = h_next
+            Y[t_count, :, layer] = y
+
+    return H, Y
