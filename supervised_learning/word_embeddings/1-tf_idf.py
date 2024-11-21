72a88192f849bc1837e32f15ae447e64c722b927
Ace <acequantumbit@gmail.com>
2024-11-21 11:39:22 -0600

    ok cool

--- supervised_learning/word_embeddings/1-tf_idf.py
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -5,7 +5,9 @@ For reference"""
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 
+
 def tf_idf(sentences, vocab=None):
+    """Documentation I forgot"""
     tfid_vec = TfidfVectorizer(vocabulary=vocab)
 
     tfid_vec.fit(sentences)
@@ -16,4 +18,4 @@ def tf_idf(sentences, vocab=None):
     # I'm actually not sure if I'm supposed to transform it
     # Lemme check task 0
     #   No
-    return(ret_array.toarray(), tfid_vec.get_feature_names_out())
+    return (ret_array.toarray(), tfid_vec.get_feature_names_out())
