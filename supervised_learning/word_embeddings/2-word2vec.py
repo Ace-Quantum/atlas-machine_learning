6862c0b17607caf350b129c0a75b561e74386e5a
Ace <acequantumbit@gmail.com>
2024-11-21 11:43:57 -0600

    back to the playground

---
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Ok this is where the difficulty spikes
+I'm gonna slam through this the same way I do everything
+Panicking"""
+
+def word2vec_model(sentences, vector_size=100,
+                   min_count=5, window=5, negative=5,
+                   cbow=True, epochs=5, seed=0, workers=1):
+    """I officially have no idea what I'm doing"""
+    return None
