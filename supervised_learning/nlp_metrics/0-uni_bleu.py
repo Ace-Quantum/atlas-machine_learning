a159322686d91e807af470965b05b12f923cca00
Ace <acequantumbit@gmail.com>
2024-11-26 15:40:27 -0600

    styling

--- supervised_learning/nlp_metrics/0-uni_bleu.py
+++ supervised_learning/nlp_metrics/0-uni_bleu.py
@@ -3,6 +3,7 @@
 
 import numpy as np
 
+
 def uni_bleu(references, sentence):
     """You need references for uni"""
 
@@ -13,17 +14,22 @@ def uni_bleu(references, sentence):
     max_ref_counts = {}
     for reference in references:
         for word in reference:
-            max_ref_counts[word] = max(max_ref_counts.get(word, 0), reference.count(word))
+            max_ref_counts[word] = max(
+                max_ref_counts.get(word, 0), reference.count(word)
+            )
 
     clipped_count = 0
     for word in sentence_counts:
-        clipped_count += min(sentence_counts[word], max_ref_counts.get(word, 0))
+        clipped_count += min(sentence_counts[word],
+                             max_ref_counts.get(word, 0))
 
     precision = clipped_count / len(sentence)
 
     sentence_len = len(sentence)
     ref_lengths = [len(ref) for ref in references]
-    closest_ref_len = min(ref_lengths, key=lambda ref_len: (abs(ref_len - sentence_len), ref_len))
+    closest_ref_len = min(
+        ref_lengths, key=lambda ref_len: (abs(ref_len - sentence_len), ref_len)
+    )
 
     if sentence_len > closest_ref_len:
         brevity_penelty = 1
