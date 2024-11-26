600bd235d3a5ff5bae95e10f803b775acb019754
Ace <acequantumbit@gmail.com>
2024-11-26 10:47:00 -0600

    point scraping

---
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""gotta go fast"""
+
+import gensim
+
+
+def fasttext_model(
+    sentences,
+    vector_size=100,
+    min_count=5,
+    negative=5,
+    window=5,
+    cbow=True,
+    epochs=5,
+    seed=0,
+    workers=1,
+):
+    """Sanic"""
+    return None
