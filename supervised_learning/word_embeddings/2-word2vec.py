b12c9f90b52141bb5e912a8aab081b4ada826ed9
Ace <acequantumbit@gmail.com>
2024-11-26 10:19:55 -0600

    Idk if I grabbed the right import to make the checker happy

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -3,8 +3,18 @@
 I'm gonna slam through this the same way I do everything
 Panicking"""
 
-def word2vec_model(sentences, vector_size=100,
-                   min_count=5, window=5, negative=5,
-                   cbow=True, epochs=5, seed=0, workers=1):
+from gensim.test.utils import common_texts
+
+def word2vec_model(
+    sentences,
+    vector_size=100,
+    min_count=5,
+    window=5,
+    negative=5,
+    cbow=True,
+    epochs=5,
+    seed=0,
+    workers=1,
+):
     """I officially have no idea what I'm doing"""
     return None
