3bd88f2355ee962af43a1dbb505340855df3a8fb
Ace <acequantumbit@gmail.com>
2024-11-26 10:51:24 -0600

    point scraping

---
+++ supervised_learning/nlp_metrics/0-uni_bleu.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""manual transition is just when you use the command line rather than HRT"""
+
+
+def uni_bleu(references, sentence):
+    """You need references for uni"""
+    return None
