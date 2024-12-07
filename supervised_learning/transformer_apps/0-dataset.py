0641387535bcae114844336bc046e9636315c118
Ace <acequantumbit@gmail.com>
2024-12-07 14:55:40 -0600

    pycodestyle

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -14,16 +14,19 @@ class Dataset:
             "ted_hrlr_translate/pt_to_en", split="train", as_supervised=True
         )
         self.data_valid = tfds.load(
-            "ted_hrlr_translate/pt_to_en", split="validation", as_supervised=True
+            "ted_hrlr_translate/pt_to_en",
+            split="validation", as_supervised=True
         )
 
-        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(self.data_train)
+        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
+            self.data_train)
 
     def tokenize_dataset(self, data):
         """Documentation"""
         tokenizer_pt = transformers.AutoTokenizer.from_pretrained(
             "neuralmind/bert-base-portuguese-cased"
         )
-        tokenizer_en = transformers.AutoTokenizer.from_pretrained("bert-base-uncased")
+        tokenizer_en = transformers.AutoTokenizer.from_pretrained(
+            "bert-base-uncased")
 
         return tokenizer_pt, tokenizer_en
