f1e151ad19d99ecd2da870fa3ef31391d12d693e
Ace <acequantumbit@gmail.com>
2025-01-07 11:30:34 -0600

    pycodestyle

--- reinforcement_learning/q_learning/0-load_env.py
+++ reinforcement_learning/q_learning/0-load_env.py
@@ -6,14 +6,15 @@ No idea."""
 
 import gymnasium as gym
 
+
 def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
     """Loading a frozen lake
-    desc - a list containing the 
+    desc - a list containing the
     description of the map for the environment
     map_name - string that is the map
     ---(????????? The assignemnt determines it to contain the map?
     ---"map_name is either None or a string containing the pre-made map to load"
-    ---That's literally what it says. 
+    ---That's literally what it says.
     ---I'm assuming the string itself doesn't contain the map.)
     You can ignore all of that I think I got my answer
     is_slippery - is the ice slippery?
@@ -22,7 +23,7 @@ def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
     Returns the environment
     """
 
-    env = gym.make('FrozenLake', desc=desc, map_name=map_name, is_slippery=is_slippery)
+    env = gym.make("FrozenLake", desc=desc, map_name=map_name, is_slippery=is_slippery)
 
     return env
 
