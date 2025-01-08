bc405834768f040f3c3175ba77ed1b22c713ea5f
Ace <acequantumbit@gmail.com>
2025-01-08 14:52:08 -0600

    Pretty sure these aren't the numbers I should be getting

--- reinforcement_learning/q_learning/3-q_learning.py
+++ reinforcement_learning/q_learning/3-q_learning.py
@@ -4,3 +4,69 @@
 
 import numpy as np
 import gymnasium as gym
+epsilon_greedy = __import__('2-epsilon_greedy').epsilon_greedy
+
+def train(env, Q, episodes=5000, max_steps=100,
+          alpha=0.1, gamma=0.99, epsilon=1, min_epsilon=0.1,
+          epsilon_decay=0.05):
+    """It all comes down to this
+    Only it actually doesn't because there's another task after this one
+    Anyway here's the variables
+    env - frozen lake environment
+    q - the q table
+    episodes - basically epochs
+    max_steps - the truncation cutoff. 
+    ---Boiyo isn't allowed any more steps than this to reach the end
+    alpha - learning rate
+    gamma - discount rate
+    epsilon - init. threshold for epsilon greedy
+    min_epsilon - the minimum value that epsilon should decay to.
+    epsilon_decay - decay rate of epsilon
+    
+    falling through hole updates the q-table to -1
+    import the epsilon greedy function we made
+    ---mine is currently not passing all the checks---
+    returns: Q, total rewards
+    Q - updated Q Table
+    total_rewards - list of rewards per episode"""
+
+    rewards_per_episode = np.zeros(episodes)
+    step_counter = 0
+
+    for i in range(episodes):
+        # print(f"for loop iteration: ", i)
+        state = env.reset()[0]
+        terminated = False
+        truncated = False
+
+        # debug_counter = 0
+        while(not terminated and not truncated):
+            # print(f"while iterator: ", debug_counter)
+            action = epsilon_greedy(Q, state, epsilon)
+
+            new_state, reward, terminated, truncated, _ = env.step(action)
+
+            Q[state, action] = Q[state, action] + alpha * (
+                reward + gamma * np.max(Q[new_state,:]) - Q[state, action]
+            )
+
+            step_counter += 1
+            state = new_state
+    
+        epsilon = max(epsilon - epsilon_decay, 0)
+
+        if(epsilon==0):
+            alpha = 0.0001
+
+        if reward == 1:
+            rewards_per_episode[i] = 1
+
+        # print(f"steps taken: ", step_counter)
+        if step_counter >= max_steps:
+            break
+
+        # print(f"epsilon: ", epsilon)
+        if epsilon <= min_epsilon:
+            break
+
+    return Q, rewards_per_episode
--- reinforcement_learning/q_learning/README.md
+++ reinforcement_learning/q_learning/README.md
@@ -1,1 +1,14 @@
 Behold, a readme
+
+Sources used:
+https://www.youtube.com/watch?v=ZhoIgo3qqLU
+^^^ Thank you David
+
+https://github.com/johnnycode8/gym_solutions/blob/main/frozen_lake_q.py
+^^ Github related to the video tutorial
+
+https://www.geeksforgeeks.org/q-learning-in-python/
+^^ Solid break down of concepts
+
+https://en.wikipedia.org/wiki/Q-learning
+^^ I have yet to read all of this but we stan Wikipedia so I had to include it.
