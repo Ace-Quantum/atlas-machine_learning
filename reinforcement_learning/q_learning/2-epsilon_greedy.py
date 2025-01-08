d555b02c35410d1de4432207b4d1e8a6d451d3ba
Ace <acequantumbit@gmail.com>
2025-01-08 14:08:01 -0600

    styling

--- reinforcement_learning/q_learning/2-epsilon_greedy.py
+++ reinforcement_learning/q_learning/2-epsilon_greedy.py
@@ -4,19 +4,20 @@
 
 import numpy as np
 
+
 def epsilon_greedy(Q, state, epsilon):
     """chosing where the state goes from here
     Q contains the q-table
     state is where the boiyo is
     epsilon is the epsilon to use for calculations
-    
+
     It says we need to sample p to determine if we explore or exploit
     It does not give us a p to sample
     Regardless we are to use numpy.random.uniform
 
     exploring should be done with numpy.random.randint
-    
-    returns the next action index 
+
+    returns the next action index
     (I assume that's the index of the new state but I could be wrong.)
         I was wrong, this is just determining explore or exploit
             Nope!!!! This is the direction our boiyo is going.
