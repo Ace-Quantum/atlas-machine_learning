0929da7b74812e3a4c180fda52054d60a7520e74
Ace <acequantumbit@gmail.com>
2025-01-11 15:16:40 -0600

    Pycodestyle

--- reinforcement_learning/q_learning/3-q_learning.py
+++ reinforcement_learning/q_learning/3-q_learning.py
@@ -3,29 +3,37 @@
 """This script holds a function that performs Q learning"""
 
 import numpy as np
-import gymnasium as gym
-epsilon_greedy = __import__('2-epsilon_greedy').epsilon_greedy
 
-def train(env, Q, episodes=5000, max_steps=100,
-          alpha=0.1, gamma=0.99, epsilon=1, min_epsilon=0.1,
-          epsilon_decay=0.05):
+epsilon_greedy = __import__("2-epsilon_greedy").epsilon_greedy
+
+
+def train(
+    env,
+    Q,
+    episodes=5000,
+    max_steps=100,
+    alpha=0.1,
+    gamma=0.99,
+    epsilon=1,
+    min_epsilon=0.1,
+    epsilon_decay=0.05,
+):
     """It all comes down to this
     Only it actually doesn't because there's another task after this one
     Anyway here's the variables
     env - frozen lake environment
     q - the q table
     episodes - basically epochs
-    max_steps - the truncation cutoff. 
+    max_steps - the truncation cutoff.
     ---Boiyo isn't allowed any more steps than this to reach the end
     alpha - learning rate
     gamma - discount rate
     epsilon - init. threshold for epsilon greedy
     min_epsilon - the minimum value that epsilon should decay to.
     epsilon_decay - decay rate of epsilon
-    
+
     falling through hole updates the q-table to -1
     import the epsilon greedy function we made
-    ---mine is currently not passing all the checks---
     returns: Q, total rewards
     Q - updated Q Table
     total_rewards - list of rewards per episode"""
@@ -39,16 +47,15 @@ def train(env, Q, episodes=5000, max_steps=100,
         truncated = False
         step_counter = 0
 
-
         # debug_counter = 0
-        while(not terminated and not truncated):
+        while not terminated and not truncated:
             # print(f"while iterator: ", debug_counter)
             action = epsilon_greedy(Q, state, epsilon)
 
             new_state, reward, terminated, truncated, _ = env.step(action)
 
             Q[state, action] = Q[state, action] + alpha * (
-                reward + gamma * np.max(Q[new_state,:]) - Q[state, action]
+                reward + gamma * np.max(Q[new_state, :]) - Q[state, action]
             )
 
             step_counter += 1
@@ -57,10 +64,10 @@ def train(env, Q, episodes=5000, max_steps=100,
             # print(f"steps taken: ", step_counter)
             if step_counter >= max_steps:
                 break
-    
+
         epsilon = max(epsilon - epsilon_decay, 0)
 
-        if(epsilon==0):
+        if epsilon == 0:
             alpha = min_epsilon
 
         if reward == 1:
@@ -68,7 +75,7 @@ def train(env, Q, episodes=5000, max_steps=100,
 
         # print(f"epsilon: ", epsilon)
         # if epsilon <= min_epsilon:
-            # break
+        # break
 
     env.close()
 
