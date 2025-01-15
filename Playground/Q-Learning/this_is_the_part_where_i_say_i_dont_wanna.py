35962d5957f31029c6451ecb7e44b7e0009331ab
Ace <acequantumbit@gmail.com>
2025-01-15 12:13:14 -0600

    Let's see if it works now

---
+++ Playground/Q-Learning/this_is_the_part_where_i_say_i_dont_wanna.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Ok so take two on this project, 
+# as the last two days were eaten by my WSL being down.
+# Fingers crossed the same doesn't happen again.
+
+# Following this tutorial from David:
+# https://www.youtube.com/watch?v=2nonlRp3vT0
+# I had initially tried to follow one using tensorflow and keras
+# But I'm assuming the support from gym with that one was depreciated.
+
+import gym
+import numpy as np
+from PIL import Image
+import torch
+
+print("ran successfully")
