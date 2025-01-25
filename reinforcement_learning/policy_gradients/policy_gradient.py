0edaf0a24a1142adebc1c3cd68d394dfc33ab0c8
Ace <acequantumbit@gmail.com>
2025-01-25 15:15:53 -0600

    For some reason I'm getting the wrong numbers?

---
+++ reinforcement_learning/policy_gradients/policy_gradient.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""Yep we still don't know what we're doing here.
+But we'll give it the old college try"""
+
+import numpy as np
+
+def policy(matrix, weight):
+    """I guess all it is is a multiplication of the matrixes"""
+    return np.dot(matrix, weight)
