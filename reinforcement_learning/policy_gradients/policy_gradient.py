eb926d97c88ea7bf5d889d9b395885c8c51febd5
Ace <acequantumbit@gmail.com>
2025-01-26 16:39:23 -0600

    I feel like this should have been easier to figure out but the language of the task is garbage.

--- reinforcement_learning/policy_gradients/policy_gradient.py
+++ reinforcement_learning/policy_gradients/policy_gradient.py
@@ -5,6 +5,12 @@ But we'll give it the old college try"""
 
 import numpy as np
 
+
 def policy(matrix, weight):
     """I guess all it is is a multiplication of the matrixes"""
-    return np.dot(matrix, weight)
+    logits = np.dot(matrix, weight)
+
+    exp_logits = np.exp(logits - np.max(logits))
+    softmax_probs = exp_logits / np.sum(exp_logits, axis=1, keepdims=True)
+
+    return softmax_probs
