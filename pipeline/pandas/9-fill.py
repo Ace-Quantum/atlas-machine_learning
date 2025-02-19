dbc6907cb4741282c24813855f02fd4480a50d7c
Ace <acequantumbit@gmail.com>
2025-02-19 13:42:42 -0600

    Time to spam the checker

--- pipeline/pandas/9-fill.py
+++ pipeline/pandas/9-fill.py
@@ -4,18 +4,20 @@
 Honestly it kind of doesn't make sense to have this project
 after the preprocessing paper project"""
 
+
 def fill(df):
     """fills in values"""
 
-    df_filled = df.drop('Weighted_Price', axis=1)
+    df_filled = df.drop("Weighted_Price", axis=1)
 
-    df_filled.loc[:, ['Close']] = df_filled.loc[:, ['Close']].ffill()
+    df_filled.loc[:, ["Close"]] = df_filled.loc[:, ["Close"]].ffill()
 
-    df_filled['High'] = df_filled['High'].fillna(df_filled['Close'])
-    df_filled['Low'] = df_filled['Low'].fillna(df_filled['Close'])
-    df_filled['Open'] = df_filled['Open'].fillna(df_filled['Close'])
+    df_filled["High"] = df_filled["High"].fillna(df_filled["Close"])
+    df_filled["Low"] = df_filled["Low"].fillna(df_filled["Close"])
+    df_filled["Open"] = df_filled["Open"].fillna(df_filled["Close"])
 
-    df_filled[['Volume_(BTC)', 'Volume_(Currency)']] = df_filled[[
-        'Volume_(BTC)', 'Volume_(Currency)']].fillna(value=0)
-    
+    df_filled[["Volume_(BTC)", "Volume_(Currency)"]] = df_filled[
+        ["Volume_(BTC)", "Volume_(Currency)"]
+    ].fillna(value=0)
+
     return df_filled
