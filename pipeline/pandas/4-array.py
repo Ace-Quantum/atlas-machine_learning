8e28767af4698c4d125a36d6c8219647c3cf57a6
Ace <acequantumbit@gmail.com>
2025-02-19 12:18:40 -0600

    Straight to the checker

---
+++ pipeline/pandas/4-array.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""Takes the tail of a df and makes it a numpy array"""
+
+import pandas as pd
+
+def array(df):
+
+    df_tail_sample = df[['High', 'Close']]
+
+    df_tail_sample = df_tail_sample.tail(n=10)
+
+    np_array = df_tail_sample.to_numpy
+
+    return np_array
