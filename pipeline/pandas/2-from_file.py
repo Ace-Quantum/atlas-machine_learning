dd06d86f9b9b3344030fbb3330ab9c46a96c0458
Ace <acequantumbit@gmail.com>
2025-02-19 11:37:14 -0600

    Straight to the checker

---
+++ pipeline/pandas/2-from_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""pulls from a file and makes a dataframe"""
+
+import pandas as pd
+
+def from_file(filename, delimiter):
+    """Pulls a file and makes a dataframe of it
+    I will not be downloading datasets to test it"""
+
+    df_return = pd.read_csv(filename, delimiter=delimiter)
+
+    return df_return
