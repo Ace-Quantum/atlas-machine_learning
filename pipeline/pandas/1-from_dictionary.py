7b0f8debaac4234ba12da2c6879a5ae816a3eb23
Ace <acequantumbit@gmail.com>
2025-02-19 11:26:39 -0600

    pycode

--- pipeline/pandas/1-from_dictionary.py
+++ pipeline/pandas/1-from_dictionary.py
@@ -5,7 +5,8 @@
 import pandas as pd
 
 
-dict_df = {"First": [0.0, 0.5, 1.0, 1.5], "Second": ["one", "two", "three", "four"]}
+dict_df = {"First": [0.0, 0.5, 1.0, 1.5],
+           "Second": ["one", "two", "three", "four"]}
 
 rows = ["A", "B", "C", "D"]
 
