4670221cadb4755ef1a17beb9ac5970045acd354
Ace <acequantumbit@gmail.com>
2025-02-19 11:25:58 -0600

    Still doing ok so far

---
+++ pipeline/pandas/1-from_dictionary.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""First time in a while that we've hardcoded something"""
+
+import pandas as pd
+
+
+dict_df = {"First": [0.0, 0.5, 1.0, 1.5], "Second": ["one", "two", "three", "four"]}
+
+rows = ["A", "B", "C", "D"]
+
+df = pd.DataFrame.from_dict(dict_df)
+
+df.index = rows
