c105caa588e63c7345ac8ad803f450a66ae3b1f0
Ace <acequantumbit@gmail.com>
2025-02-19 14:09:19 -0600

    Seriously wth is happening

--- pipeline/pandas/11-concat.py
+++ pipeline/pandas/11-concat.py
@@ -13,7 +13,7 @@ def concat(df1, df2):
     df1 = index(df1)
     df2 = index(df2)
 
-    df2 = df2.truncate(after=1417411920)
+    # df2 = df2.truncate(after=1417411920)
 
     df_concat = pd.concat([df2, df1])
 
