e4428d808420c153ccfc864b129dbf4299cebdc5
Ace <acequantumbit@gmail.com>
2025-02-19 14:41:11 -0600

    bro i don't even know

---
+++ pipeline/pandas/13-analyze.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""Had to skip the last 2
+Let's hope that they're not worth too much"""
+
+
+def analyze(df):
+    """computes descriptive statistics for columns"""
+
+    return df.describe(exclude=['Timestamp'])
