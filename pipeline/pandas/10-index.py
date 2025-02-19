1dc373786e9ea38551a1cd827c92b0c5b4de46c1
Ace <acequantumbit@gmail.com>
2025-02-19 13:46:16 -0600

    This is still pretty fun. Despite my legs being asleep

---
+++ pipeline/pandas/10-index.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""After that last one it's kind of insulting
+to be given a 2 bullet point task"""
+
+def index(df):
+    """sets timestamp as the index"""
+
+    df.set_index('Timestamp')
+
+    return df
