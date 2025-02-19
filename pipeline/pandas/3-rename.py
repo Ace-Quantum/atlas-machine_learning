da049578c28a3b0d636b79f55e1b169359e395b0
Ace <acequantumbit@gmail.com>
2025-02-19 11:45:39 -0600

    I think I did that right?

---
+++ pipeline/pandas/3-rename.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""renames things"""
+
+import pandas as pd
+
+def rename(df):
+    """renames Timestamp to Datetime
+    convers timestamps to datetime values
+    only returns datetime and close columns"""
+
+    df_return = df.rename(columns={"Timestamp": "Datetime"})
+
+    df_return = df_return.to_datetime(df_return['Datetime'])
+
+    return(df_return)
