be16a93bc81cb4edb7b0408fb82fec6a1cdefb16
Ace <acequantumbit@gmail.com>
2025-02-19 14:45:28 -0600

    ok let's just see if that runs

--- pipeline/pandas/13-analyze.py
+++ pipeline/pandas/13-analyze.py
@@ -7,6 +7,8 @@ Let's hope that they're not worth too much"""
 def analyze(df):
     """computes descriptive statistics for columns"""
 
+    df_stats = df.drop('Timestamp', axis=1)
+
     stats = df.describe()
 
     return stats
