edbac44ba1ed57f3c25fbb123df5cf6905303a68
Ace <acequantumbit@gmail.com>
2025-02-19 12:43:49 -0600

    Not testing locally is biting me a little bit

--- pipeline/pandas/6-flip_switch.py
+++ pipeline/pandas/6-flip_switch.py
@@ -6,7 +6,7 @@
 def flip_switch(df):
     """sort in reverse chronological and then transpose"""
 
-    df_flip_switch = df.sort_values(ascending=False)
+    df_flip_switch = df.sort_values(by=['Datetime'], ascending=False)
 
     df_flip_switch = df_flip_switch.T
 
