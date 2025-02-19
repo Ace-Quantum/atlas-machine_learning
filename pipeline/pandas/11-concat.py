87516c3ea093b1b3a486bd4f54832f4153c805de
Ace <acequantumbit@gmail.com>
2025-02-19 14:05:12 -0600

    I'm genuinely not sure what's happening here

--- pipeline/pandas/11-concat.py
+++ pipeline/pandas/11-concat.py
@@ -10,8 +10,8 @@ def concat(df1, df2):
     """concats based on Timestamp
     I already wrote the set index like that so I'm leaving it"""
 
-    df1 = df1.set_index('Timestamp')
-    df2 = df2.set_index('Timestamp')
+    # df1 = df1.set_index('Timestamp')
+    # df2 = df2.set_index('Timestamp')
 
     # df2 = df2.truncate(after=1417411920)
 
