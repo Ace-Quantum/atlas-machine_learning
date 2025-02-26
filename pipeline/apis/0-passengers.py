19a50671301c88155f54e3689d145d5500f3de7f
Ace <acequantumbit@gmail.com>
2025-02-26 11:15:24 -0600

    Initial push

---
+++ pipeline/apis/0-passengers.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""We're starting work with APIs and I'm not sure how it's going to go"""
+
+import requests
+
+
+def availableShips(passengerCount):
+    """Also I just want to say how much I hate that we're working with
+    the Starwars API
+    Nothing against the project just everything against Starwars."""
+
+    ret_list = []
+
+    # This is where I started to worry about pagination
+    # I've decided to worry about it later
+
+    return ret_list
---
+++ pipeline/apis/README.md
@@ -0,0 +1,2 @@
+I remember doing this in T2
+But I do not remember how to do it
