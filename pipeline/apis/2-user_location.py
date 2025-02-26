f0c751a9d07bce2e523206bd352a21e9cbeb6ae4
Ace <acequantumbit@gmail.com>
2025-02-26 12:36:26 -0600

    One last check before a 10

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,3 +1,37 @@
 #!/usr/bin/env python3
 
 """This uses the github api"""
+
+import sys
+import requests
+from datetime import datetime
+
+def get_user_location(url):
+    """finds a user"""
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        user_data = response.json()
+        return user_data.get("location", "Location not provided")
+    elif response.status_code == 404:
+        return "Not found"
+    elif response.status_code == 403:
+        reset_timestamp = int(response.headers.get(
+            "X-Ratelimit-Reset", 0
+        ))
+
+        reset_time = datetime.fromtimestamp(reset_timestamp)
+        current_time = datetime.now()
+        minutes_until_reset = (reset_time - current_time).seconds // 60
+        return f"Reset in {minutes_until_reset} min"
+    else:
+        return f"Unexpected status code: {response.status_code}"
+    
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: ./2-user_location.py <GitHub API URL>")
+        sys.exit(1)
+
+    url = sys.argv[1]
+    location = get_user_location(url)
+    print(location)
