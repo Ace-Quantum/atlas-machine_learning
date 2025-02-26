eb9e0de83e10329daf45f4eafb18dab160f50700
Ace <acequantumbit@gmail.com>
2025-02-26 11:32:11 -0600

    Alrighty let's see how that goes:

--- pipeline/apis/0-passengers.py
+++ pipeline/apis/0-passengers.py
@@ -10,9 +10,22 @@ def availableShips(passengerCount):
     the Starwars API
     Nothing against the project just everything against Starwars."""
 
+    url = "https://swapi.dev/api/starships/"
     ret_list = []
 
     # This is where I started to worry about pagination
     # I've decided to worry about it later
+
+    while url:
+        response = requests.get(url)
+        data = response.json()
+
+        for ship in data['results']:
+            passengers = int(ship['passengers'].replace(',', ''))
+
+        if passengers >= passengerCount:
+            ret_list.append(ship['name'])
+
+        url = data['next']
 
     return ret_list
--- pipeline/apis/4-rocket_frequency.py
+++ pipeline/apis/4-rocket_frequency.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
 
 """Still working with SpaceX
-Even if it's unofficial"""
+Even if it's unofficial
+Did you hear they're being sued by Cards Against Humanity?"""
