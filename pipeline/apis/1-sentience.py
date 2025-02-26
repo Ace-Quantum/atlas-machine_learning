bc2443ea98b8a55e67ae85f1f2103a9eca290b63
Ace <acequantumbit@gmail.com>
2025-02-26 12:18:46 -0600

    I keep forgetting there's a hotkey to get to the next open page

--- pipeline/apis/1-sentience.py
+++ pipeline/apis/1-sentience.py
@@ -2,7 +2,29 @@
 
 """Returns planets with sentient species"""
 
+import requests
+
 def sentientPlanets():
     """I don't even know what pagination refers to"""
 
-    return None
+    url = "https://swapi.dev/api/species/"
+    planets = set()
+
+    while url:
+        response = requests.get(url)
+        data = response.json()
+
+        for species in data['results']:
+            is_sentient = (
+                species['classification'].lower() == 'sentient' or
+                species['designation'].lower() == 'sentient'
+            )
+
+            if is_sentient and species['homeworld']:
+                homeworld_response = requests.get(species['homeworld'])
+                homeworld_data = homeworld_response.json()
+                planets.add(homeworld_data['name'])
+
+        url = data['next']
+
+    return list(planets)
