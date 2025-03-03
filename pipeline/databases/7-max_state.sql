36b337b7003171f64d522579f64b2b61c8a60403
Ace <acequantumbit@gmail.com>
2025-03-03 13:28:46 -0600

    Grabbed by state

--- pipeline/databases/7-max_state.sql
+++ pipeline/databases/7-max_state.sql
@@ -1,6 +1,6 @@
 -- And I'm not going to be able to
 
-SELECT city, MAX(value) as avg_temp
+SELECT state, MAX(value) as max_temp
 FROM temperatures
-GROUP BY city
-ORDER BY city ASC;
+GROUP BY state
+ORDER BY state ASC;
