0eed7de17f0a05a06ba74de13d57b214fb611922
Ace <acequantumbit@gmail.com>
2025-03-03 13:10:32 -0600

    We might be missing a group by but we'll see

--- pipeline/databases/6-avg_temperatures.sql
+++ pipeline/databases/6-avg_temperatures.sql
@@ -1,1 +1,8 @@
 -- They're giving us a database to download
+-- Ok so not only do I NOT want to do this
+-- But I can't
+-- Like, my computer won't install MySQL
+-- So that's annoying
+
+SELECT city, AVG(value) as avg_temp
+FROM temperatures;
