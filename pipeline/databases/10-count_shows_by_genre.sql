9b0dcf121636f3e00e0ba76ce80e06af4cb6b543
Ace <acequantumbit@gmail.com>
2025-03-03 13:35:20 -0600

    old code

--- pipeline/databases/10-count_shows_by_genre.sql
+++ pipeline/databases/10-count_shows_by_genre.sql
@@ -1,1 +1,9 @@
 -- list all genres and the number of shows that fall under them
+
+SELECT tv_genres.name AS genre,
+COUNT(tv_show_genres.genre_id) AS number_of_shows
+FROM tv_show_genres
+INNER JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
