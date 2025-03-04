08af7454aed9e15efd5261633816114fe4483a1b
Ace <acequantumbit@gmail.com>
2025-03-04 13:23:52 -0600

    p l e a s e

--- pipeline/databases/11-rating_shows.sql
+++ pipeline/databases/11-rating_shows.sql
@@ -1,6 +1,6 @@
 -- Annnnd now we're downloading the rotten tomatoes database
 
-SELECT title, tv_show_ratings.rate
+SELECT tv_shows.title, tv_show_ratings.rate
 -- Rating = SUM(tv_show_ratings)
 FROM tv_show_ratings;
 JOIN tv_show_ratings ON tv_shows.id=tv_show_ratings.show_id;
