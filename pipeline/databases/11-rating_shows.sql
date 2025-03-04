6a338aa5a950bc98bf7ec87b66a0d5a323029d44
Ace <acequantumbit@gmail.com>
2025-03-04 11:34:26 -0600

    I'm not even sure this will run

--- pipeline/databases/11-rating_shows.sql
+++ pipeline/databases/11-rating_shows.sql
@@ -1,1 +1,5 @@
 -- Annnnd now we're downloading the rotten tomatoes database
+
+SELECT tv_shows.title, tv_show_ratings.rate
+FROM tv_show_ratings
+INNER JOIN tv_shows ON tv_show_ratings.show_id=tv_shows.id;
