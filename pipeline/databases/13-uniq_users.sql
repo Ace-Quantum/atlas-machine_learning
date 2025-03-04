b13fca23d137ebfb4ac00bb2d110825e19215278
Ace <acequantumbit@gmail.com>
2025-03-04 15:01:32 -0600

    give it a go

--- pipeline/databases/13-uniq_users.sql
+++ pipeline/databases/13-uniq_users.sql
@@ -1,1 +1,10 @@
 -- script that creates a table of users
+
+DROP TABLE IF EXISTS 'users';
+
+CREATE TABLE users(
+    id int NOT NULL AUTO_INCREMENT,
+    email varchar(255) NOT NULL UNIQUE,
+    name varchar(255),
+    PRIMARY KEY (id)
+);
