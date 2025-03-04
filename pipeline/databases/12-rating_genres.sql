42611778612e9be52875a942424712a9a4f4dc81
Ace <acequantumbit@gmail.com>
2025-03-04 14:46:53 -0600

    output achieved

--- pipeline/databases/12-rating_genres.sql
+++ pipeline/databases/12-rating_genres.sql
@@ -1,6 +1,7 @@
 -- There's yet another rating database
 
-SELECT tv_genres.name, tv_show_ratings.rate
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
 FROM tv_show_ratings
 JOIN tv_show_genres ON tv_show_genres.show_id=tv_show_ratings.show_id
-JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id;
+JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id
+GROUP BY name;
