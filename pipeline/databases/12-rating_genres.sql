dcb2e0e04bac9e4402894ff07335dbfeadc11eac
Ace <acequantumbit@gmail.com>
2025-03-04 14:23:34 -0600

    checking for output

--- pipeline/databases/12-rating_genres.sql
+++ pipeline/databases/12-rating_genres.sql
@@ -2,4 +2,5 @@
 
 SELECT tv_genres.name, tv_show_ratings.rate
 FROM tv_genres
-JOIN tv_show_genres ON tv_show_genres.show_id=tv_show_ratings.show_id;
+JOIN tv_show_genres ON tv_show_genres.show_id=tv_show_ratings.show_id
+JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id;
