75cf31b6d7f3e00f3f237a1a7323397def0d8d5d
Ace <acequantumbit@gmail.com>
2025-03-04 12:30:17 -0600

    please

--- pipeline/databases/11-rating_shows.sql
+++ pipeline/databases/11-rating_shows.sql
@@ -3,4 +3,4 @@
 SELECT title, tv_show_ratings.rate
 -- Rating = SUM(tv_show_ratings)
 FROM tv_shows;
-INNER JOIN tv_shows ON tv_show_ratings.show_id=tv_shows.id;
+INNER JOIN tv_show_ratings ON tv_show_ratings.show_id=tv_shows.id;
