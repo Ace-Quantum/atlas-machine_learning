3d20bfc3315bd146d4a9feaf40277aee680c6f6e
Ace <acequantumbit@gmail.com>
2025-03-04 12:23:41 -0600

    so far so good

--- pipeline/databases/11-rating_shows.sql
+++ pipeline/databases/11-rating_shows.sql
@@ -4,4 +4,4 @@ SELECT title
 -- , rating
 -- Rating = SUM(tv_show_ratings)
 FROM tv_shows;
--- INNER JOIN tv_shows ON tv_show_ratings.show_id=tv_shows.id;
+INNER JOIN tv_shows ON tv_show_ratings.show_id=tv_shows.id;
