a6b21bc7d1cf91e3cd3214e042c392ab1ba2dffa
Ace <acequantumbit@gmail.com>
2025-03-04 12:04:40 -0600

    trying

--- pipeline/databases/11-rating_shows.sql
+++ pipeline/databases/11-rating_shows.sql
@@ -2,5 +2,5 @@
 
 SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
 FROM tv_shows
-GROUP BY tv_shows.title
-INNER JOIN tv_shows ON tv_show_ratings.show_id=tv_shows.id;
+-- GROUP BY tv_shows.title
+INNER JOIN tv_show_ratings ON tv_show_ratings.show_id=tv_shows.id;
