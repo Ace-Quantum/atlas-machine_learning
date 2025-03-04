b932848a3570667b2666cc828b5bcc31443da5ba
Ace <acequantumbit@gmail.com>
2025-03-04 11:36:03 -0600

    I'm not even sure this will run

--- pipeline/databases/11-rating_shows.sql
+++ pipeline/databases/11-rating_shows.sql
@@ -1,5 +1,6 @@
 -- Annnnd now we're downloading the rotten tomatoes database
 
 SELECT tv_shows.title, tv_show_ratings.rate
+SUM(tv_show_ratings.rate) AS rating
 FROM tv_show_ratings
 INNER JOIN tv_shows ON tv_show_ratings.show_id=tv_shows.id;
