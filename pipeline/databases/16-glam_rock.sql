8e81b9f503c849873c469cad8a95b33a40fbb01a
Ace <acequantumbit@gmail.com>
2025-03-05 16:58:46 -0600

    I'm not quite sure if this is how wildcards work

--- pipeline/databases/16-glam_rock.sql
+++ pipeline/databases/16-glam_rock.sql
@@ -1,1 +1,10 @@
 -- pull all bands considered glam rock and organize them
+
+SELECT band_name
+CASE
+WHEN slpit IS NULL THEN 2020 - formed
+ELSE slpit - formed
+END AS `lifespan until 2020 (in years)`
+FROM metal_bands
+WHERE style = '*Glam rock*'
+ORDER BY lifespan until 2020 (in years) DESC; 
