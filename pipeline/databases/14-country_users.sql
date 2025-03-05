04dcd34f77a5bee8005a4c5d8e4f7f1802af95cc
Ace <acequantumbit@gmail.com>
2025-03-05 16:32:57 -0600

    Let's see

--- pipeline/databases/14-country_users.sql
+++ pipeline/databases/14-country_users.sql
@@ -1,2 +1,12 @@
 -- creates the table "user" which probably 
--- should have happened before that last one
+-- should have happened before that last one-- script that creates a table of users
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id int NOT NULL AUTO_INCREMENT,
+    email varchar(255) NOT NULL UNIQUE,
+    name varchar(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
+    PRIMARY KEY (id)
+);
