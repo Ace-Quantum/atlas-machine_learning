e36545a8cd72025ebc64f1018925302b94822e38
Ace <acequantumbit@gmail.com>
2025-03-05 17:55:50 -0600

    please?

--- pipeline/databases/16-glam_rock.sql
+++ pipeline/databases/16-glam_rock.sql
@@ -2,6 +2,6 @@
 
 SELECT band_name
     -- split - formed AS lifespan
-FROM metal_bands;
--- WHERE style = '%Glam rock%';
+FROM metal_bands
+WHERE style = '%Glam rock%';
 -- ORDER BY lifespan DESC; 
