44b228a9e26d9d789a0a7f364bd2e1f03cffffc1
Ace <acequantumbit@gmail.com>
2025-03-05 17:00:45 -0600

    try again

--- pipeline/databases/16-glam_rock.sql
+++ pipeline/databases/16-glam_rock.sql
@@ -6,5 +6,5 @@ WHEN slpit IS NULL THEN 2020 - formed
 ELSE slpit - formed
 END AS `lifespan until 2020 (in years)`
 FROM metal_bands
-WHERE style = '*Glam rock*'
-ORDER BY lifespan until 2020 (in years) DESC; 
+WHERE style = '%Glam rock%'
+ORDER BY `lifespan until 2020 (in years)` DESC; 
