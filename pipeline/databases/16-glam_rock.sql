2a13d28630d16fe70e046d890f1180359d488c6e
Ace <acequantumbit@gmail.com>
2025-03-05 17:06:53 -0600

    literally anything

--- pipeline/databases/16-glam_rock.sql
+++ pipeline/databases/16-glam_rock.sql
@@ -5,6 +5,6 @@ SELECT band_name
 -- WHEN split IS NULL THEN 2020 - formed
 -- ELSE split - formed
 -- END AS `lifespan until 2020 (in years)`
-FROM metal_bands
-WHERE style = 'Glam rock';
+FROM metal_bands;
+-- WHERE style = 'Glam rock';
 -- ORDER BY `lifespan until 2020 (in years)` DESC; 
