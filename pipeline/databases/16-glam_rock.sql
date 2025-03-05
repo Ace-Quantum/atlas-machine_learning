dbf375b80f1ed0d35e6d1215e725bf45bee1570b
Ace <acequantumbit@gmail.com>
2025-03-05 17:51:06 -0600

    please?

--- pipeline/databases/16-glam_rock.sql
+++ pipeline/databases/16-glam_rock.sql
@@ -1,7 +1,7 @@
 -- pull all bands considered glam rock and organize them
 
 SELECT band_name, 
-    split - formed AS lifespan
+    split - formed AS 'lifespan'
 FROM metal_bands
 WHERE style = '%Glam rock%'
 ORDER BY lifespan DESC; 
