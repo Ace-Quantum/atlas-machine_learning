c2a5afeac608a569b847d6c5d2272d10f84a31b0
Ace <acequantumbit@gmail.com>
2025-03-05 17:03:08 -0600

    checking for output

--- pipeline/databases/16-glam_rock.sql
+++ pipeline/databases/16-glam_rock.sql
@@ -1,10 +1,10 @@
 -- pull all bands considered glam rock and organize them
 
 SELECT band_name
-CASE
-WHEN split IS NULL THEN 2020 - formed
-ELSE split - formed
-END AS `lifespan until 2020 (in years)`
+-- CASE
+-- WHEN split IS NULL THEN 2020 - formed
+-- ELSE split - formed
+-- END AS `lifespan until 2020 (in years)`
 FROM metal_bands
 WHERE style = '%Glam rock%'
-ORDER BY `lifespan until 2020 (in years)` DESC; 
+-- ORDER BY `lifespan until 2020 (in years)` DESC; 
