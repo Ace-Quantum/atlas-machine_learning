bd8fc0fd491877147fa603af00a85f77f5685a52
Ace <acequantumbit@gmail.com>
2025-03-05 17:01:25 -0600

    I was spelling split wrong

--- pipeline/databases/16-glam_rock.sql
+++ pipeline/databases/16-glam_rock.sql
@@ -2,8 +2,8 @@
 
 SELECT band_name
 CASE
-WHEN slpit IS NULL THEN 2020 - formed
-ELSE slpit - formed
+WHEN split IS NULL THEN 2020 - formed
+ELSE split - formed
 END AS `lifespan until 2020 (in years)`
 FROM metal_bands
 WHERE style = '%Glam rock%'
