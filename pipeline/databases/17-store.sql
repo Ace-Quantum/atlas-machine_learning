f4b251bd9e6a42a56cd874e79f6b2a134666882d
Ace <acequantumbit@gmail.com>
2025-03-05 20:45:09 -0600

    coming down to the wire

--- pipeline/databases/17-store.sql
+++ pipeline/databases/17-store.sql
@@ -2,15 +2,13 @@
 -- Not sure how
 -- Does it just like... automatically happen?
 
-DELIMITER //
+-- DELIMITER //
 
 CREATE TRIGGER after_order_insert
 AFTER INSERT ON orders
 FOR EACH ROW
-BEGIN
     UPDATE items
     SET quantity = quantity - NEW.number
-    WHERE name = NEW.name;
-END //
+    WHERE items.name = NEW.item_name;
 
-DELIMITER ;
+-- DELIMITER ;
