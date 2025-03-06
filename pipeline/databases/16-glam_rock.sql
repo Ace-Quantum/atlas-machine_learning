4697db3856d0ce82a4a0f985f8ea98f8127d11f1
Ace <acequantumbit@gmail.com>
2025-03-05 20:07:15 -0600

    ok let's try something else then

--- pipeline/databases/16-glam_rock.sql
+++ pipeline/databases/16-glam_rock.sql
@@ -1,7 +1,14 @@
 -- pull all bands considered glam rock and organize them
 
-SELECT band_name
-    -- split - formed AS lifespan
+SELECT band_name,
+  CASE
+    WHEN `split` IS NULL THEN 2020
+    WHEN split < 2020 THEN split
+    ELSE 2020
+  END
+    - formed
+  AS lifespan
+
 FROM metal_bands
-WHERE style = '%Glam rock%';
--- ORDER BY lifespan DESC; 
+WHERE style = '%Glam rock%'
+ORDER BY lifespan DESC; 
