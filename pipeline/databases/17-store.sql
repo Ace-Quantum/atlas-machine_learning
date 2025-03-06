699af871a03f1dcbccb7ff5581bcc35046453d06
Ace <acequantumbit@gmail.com>
2025-03-05 20:39:24 -0600

    coming down to the wire

--- pipeline/databases/17-store.sql
+++ pipeline/databases/17-store.sql
@@ -1,3 +1,16 @@
 -- This task is telling me to let MySQL handle the deletion of data
 -- Not sure how
 -- Does it just like... automatically happen?
+
+DELIMITER //
+
+CREATE TRIGGER after_order_insert
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.name
+END //
+
+DELIMITER ;
