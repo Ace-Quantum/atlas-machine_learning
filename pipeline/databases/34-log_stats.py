2edc0569bc51987b25869010c4efe4be3fb9a24a
Ace <acequantumbit@gmail.com>
2025-03-05 21:24:38 -0600

    my cat is driving me nuts

--- pipeline/databases/34-log_stats.py
+++ pipeline/databases/34-log_stats.py
@@ -2,3 +2,27 @@
 
 """provides stats about Nginx logs"""
 
+from pymongo import MongoClient
+
+def nginx_log_stats():
+    """gets some stats"""
+
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client['logs']
+    collection = db['nginx']
+
+    total_logs = collection.count_documents({})
+    print(f"{total_logs} logs")
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for method in methods:
+        count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+    status_count = collection.count_documents({"method": "GET",
+                                               "path": "/status"})
+    print(f"{status_count} logs with method=GET and path=/status")
+
+if __name__ == "__main__":
+    nginx_log_stats()
