5301a6950393da6b7afb0d440c75ca158df0a64c
Ace <acequantumbit@gmail.com>
2025-03-05 21:07:30 -0600

    Ok but these are actually easier than anticipated?

--- pipeline/databases/32-update_topics.py
+++ pipeline/databases/32-update_topics.py
@@ -7,4 +7,9 @@ from pymongo import MongoClient
 
 def update_topics(mongo_collection, name, topics):
     """Good golly this is a lot of files"""
-    return None
+    result = mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
+
+    return result
