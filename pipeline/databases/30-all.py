f4402bf904c5528ad4a5d614954721166dd4eb95
Ace <acequantumbit@gmail.com>
2025-03-05 21:00:45 -0600

    we imported mongo client but used mongo collection?

--- pipeline/databases/30-all.py
+++ pipeline/databases/30-all.py
@@ -7,4 +7,6 @@ from pymongo import MongoClient
 
 def list_all(mongo_collection):
     """Not sure what else to say"""
-    return None
+    documents = list(mongo_collection.find({}))
+
+    return documents
